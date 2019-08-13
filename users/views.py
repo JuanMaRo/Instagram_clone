f11c1966e1c4ecba76dbca8a0e669879beb129ce
Juan Márquez <marquez.cucho@gmail.com>
2019-08-13 16:42:31 -0500

    Validación de formularios

--- users/views.py
+++ users/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth.models import User
 from users.models import Profile
 
 # Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 
 @login_required
@@ -70,37 +70,18 @@ def login_view(request):
 def signup(request):
     """Sign up view."""
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
 
-        if passwd != passwd_confirmation:
-            return render(
-                request,
-                'users/signup.html',
-                {'error': 'Password confirmation does not match'}
-            )
-
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-        except IntegrityError:
-            return render(
-                request,
-                'users/signup.html',
-                {'error': 'Username is already in user'}
-            )
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
+    return render(
+        request=request,
+        tempalte_name='users/signup.html',
+        context={'form': form}
+    )
 
 
 @login_required
