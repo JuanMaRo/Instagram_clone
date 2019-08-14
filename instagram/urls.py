d0b2243147d3c3a0e969b8f7bf073b9b8d8b2fdc
Juan Márquez <marquez.cucho@gmail.com>
2019-08-14 09:44:02 -0500

    URLs moduladas

--- instagram/urls.py
+++ instagram/urls.py
@@ -16,25 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
-
-from .views import hello_world, sort_integers, say_hi
-from posts import views as posts_views
-from users import views as users_views
+from django.urls import path, include
 
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    path('hello-world/', hello_world, name='hello_world'),
-    path('sorted/', sort_integers, name='sort'),
-    path('hi/<str:name>/<int:age>', say_hi, name='hi'),
 
-    path('', posts_views.list_posts, name='feed'),
-    path('posts/new/', posts_views.create_post, name='create_post'),
-
-    path('users/login/', users_views.login_view, name='login'),
-    path('users/logout/', users_views.logout_view, name='logout'),
-    path('users/signup/', users_views.signup, name='signup'),
-    path('users/me/profile/', users_views.update_profile, name='update_profile'),
+    path('', include(('posts.urls', 'posts'), namespace='posts')),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
